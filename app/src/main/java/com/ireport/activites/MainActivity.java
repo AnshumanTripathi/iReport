4df2dfcea5c2f83ad4c02998b542bd6a3e403601
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2016-11-28 15:12:47 -0800

    Removed aws testing code

--- app/src/main/java/com/ireport/activites/MainActivity.java
+++ app/src/main/java/com/ireport/activites/MainActivity.java
@@ -21,36 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initElements();
-        // Set your request method, path, query string parameters, and request body
-        final String method = "POST";
-        final String path = "/items";
-        final String body = "{\"someParameter\":\"someValue\"}";
-        final Map<String, String> queryStringParameters = new HashMap<String, String>();
-        final Map<String, String> headers = new HashMap<String, String>();
-
-        //Construct the request
-        final ApiRequest request =
-                new ApiRequest(this.getClass().getSimpleName())
-                        .withPath(path)
-                        .withHttpMethod(HttpMethodName.valueOf(method))
-                        .withHeaders(headers)
-                        .addHeader("Content-Type"s "application/json")
-                        .addHeader("Content-Length", String.valueOf(content.length))
-                        .withParameters(queryStringParameters)
-                        .withBody(content);
-
-        // Create an instance of your custom SDK client
-        final AWSMobileClient mobileClient = AWSMobileClient.defaultMobileClient();
-        final CloudLogicAPI client = mobileClient.createAPIClient(MyAPImobilehubClient.class);
-        final byte[] content = body.getBytes(StringUtils.UTF8);
-
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        });
     }
 
     private void initElements(){
