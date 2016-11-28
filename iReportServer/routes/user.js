2d2677f3fe8e6a8a44a78813ca4963d5356702c6
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2016-11-28 13:42:50 -0800

    updated db and collection name

--- iReportServer/routes/user.js
+++ iReportServer/routes/user.js
@@ -5,12 +5,12 @@ var client = require('mongodb').MongoClient;
 var ec2 = "ec2-35-164-176-121.us-west-2.compute.amazonaws.com";
 
 exports.findUser = function (req, res) {
-    client.connect("mongodb://"+ec2+":27017/test", function (err, db) {
+    client.connect("mongodb://"+ec2+":27017/iReport", function (err, db) {
         if (err) {
             console.log("Error Occured");
         } else {
             console.log("Connected to MongoDB. Fetching Data....");
-            var collection = db.collection("testcoll");
+            var collection = db.collection("user");
             collection.find().toArray(function (err, results) {
                 console.log("in find");
                 if (err) {
