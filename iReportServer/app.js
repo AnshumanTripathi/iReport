c09880cd3bdc57cc337607827fe6cd8fc5463b23
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2016-11-28 14:38:33 -0800

    User related apis

--- iReportServer/app.js
+++ iReportServer/app.js
@@ -18,12 +18,14 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.post('/find',user.findUser);
+app.post('/getAllUsers',user.getAllUsers);
+app.post('/getUser',user.getUser);
+app.post('/updateSettings',user.updateSettings);
 
 
 // catch 404 and forward to error handler
--- iReportServer/routes/user.js
+++ iReportServer/routes/user.js
@@ -4,8 +4,8 @@
 var client = require('mongodb').MongoClient;
 var ec2 = "ec2-35-164-176-121.us-west-2.compute.amazonaws.com";
 
-exports.findUser = function (req, res) {
-    client.connect("mongodb://"+ec2+":27017/iReport", function (err, db) {
+exports.getAllUsers = function (req, res) {
+    client.connect("mongodb://" + ec2 + ":27017/iReport", function (err, db) {
         if (err) {
             console.log("Error Occured");
         } else {
@@ -18,8 +18,60 @@ exports.findUser = function (req, res) {
                 } else {
                     console.log(results);
                     res.send(results);
+                    db.close();
                 }
             });
+        }
+    });
+};
+
+exports.getUser = function (req, res) {
+    var query = req.body.email;
+    console.log("Email received: " + query);
+    client.connect("mongodb://" + ec2 + ":27017/iReport", function (err, db) {
+        if (err) {
+            console.log("Error Occured: " + err);
+        } else {
+            console.log("Connected to MongoDB. Fetching Data....");
+            var collection = db.collection("user");
+            collection.findOne({"email": "" + query}, function (err, results) {
+                if (err) {
+                    console.log("Error in find: " + err);
+                } else {
+                    console.log(results);
+                    res.send(results);
+                    db.close();
+                }
+            });
+        }
+    });
+};
+
+exports.updateSettings = function (req, res) {
+    var query = req.body.email;
+    console.log("Email received: " + query);
+    client.connect("mongodb://" + ec2 + ":27017/iReport", function (err, db) {
+        if (err) {
+            console.log("Error Occured: " + err);
+        } else {
+            console.log("Connected to MongoDB. Fetching Data....");
+            var collection = db.collection("user");
+            collection.updateOne({"email": "" + query},
+                {
+                    $set: {
+                        "email_confirm": req.body.email_confirm,
+                        "email_notify": req.body.email_notify,
+                        "anonymous": req.body.anonymous
+                    }
+                }, function (err, results) {
+                    if (err) {
+                        console.log("Error in find: " + err);
+                    } else {
+                        console.log(results);
+                        res.send(results);
+                        db.close();
+                    }
+                });
         }
     });
 };
