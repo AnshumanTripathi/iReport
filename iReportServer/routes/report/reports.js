1b6be4082aed2f717165ae82f157256ecf4232d2
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2016-12-04 12:48:46 -0800

    Fixed app report issue

--- iReportServer/routes/report/reports.js
+++ iReportServer/routes/report/reports.js
@@ -6,29 +6,30 @@ var User = require('../schema/UserSchema');
 
 exports.addReport = function (req, res) {
     var report = new Report(req.body);
+    var jsonResponse;
     report.set('status', 'still_there');
     console.log(report);
     report.save(function (err, report) {
         if (err) {
             console.log("Error occured while connecting saving report:" + err);
             res.send({
-                statusCode: 400,
+                statusCode: 500,
                 data: err
             });
         } else {
             User.findOne({"email": report.user_email}, function (err, user) {
                 if (err) {
                     console.log("Error Occured retrieving user data: " + err);
-                    res.send({
+                    jsonResponse = {
                         statusCode: 500,
                         data: err
-                    });
+                    };
                 } else if (user == null) {
-                    console.log("No user found with the given email: " + err);
-                    res.send({
+                    console.log("No user found with the given email: " + report.user_email);
+                    jsonResponse = {
                         statusCode: 400,
-                        data: err
-                    });
+                        data: "No user found with the given email: " + report.user_email
+                    };
                 } else {
                     if (!user.settings.anonymous) {
                         report.isAnonymous = false;
@@ -41,27 +42,27 @@ exports.addReport = function (req, res) {
                     report.save(function (err) {
                         if (err) {
                             console.log("Error Occured in Report saving: " + err);
-                            res.send({
+                            jsonResponse = {
                                 statusCode: 500,
                                 data: err
-                            });
+                            };
                         }
                     });
                     user.save(function (err) {
                         if (err) {
                             console.log("Error occured: " + err);
-                            res.send({
+                            jsonResponse = {
                                 statusCode: 500,
                                 data: err
-                            });
+                            };
                         }
                     });
+                    jsonResponse = {
+                        statusCode: 200,
+                        data: "Report Added"
+                    };
                 }
-            });
-            console.log("Report Added");
-            res.send({
-                statusCode: 200,
-                data: "Report Added"
+                res.send(jsonResponse);
             });
         }
     });
