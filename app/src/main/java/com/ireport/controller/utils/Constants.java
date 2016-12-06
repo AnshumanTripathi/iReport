8a81b906fad18d4d7b06fce8f14b553f8a53d609
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2016-12-06 14:54:43 -0800

    Merging Values

--- app/src/main/java/com/ireport/controller/utils/Constants.java
+++ app/src/main/java/com/ireport/controller/utils/Constants.java
@@ -6,12 +6,12 @@ package com.ireport.controller.utils;
 
 public class Constants {
     //final public static String SERVER_URL = "http://192.168.19.1";
-     final public static String SERVER_URL = "http://192.168.0.19";
+     final public static String SERVER_URL = "http://10.250.131.223";
     //final public static String SERVER_URL = "http://192.168.0.108";
     final public static String SERVER_PORT = "3000";
 
     // email id: dont harcode
-    final public static String SANDHYA_EMAIL = "sandhyafeb1990@gmail.com";
+    final public static String SANDHYA_EMAIL = "somyaaggarwal@ymail.com";
     final public static String MR_SANJAY_DUTT_EMAIL = "sanjay_dutt@gmail.com";
 
     final public static double DISTANCE_TO_UPDATE_REPORT = 30.0;
