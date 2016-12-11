16ac34b64199c1d56563344d927225fb67a59598
aggarwalsomya <aggarwal.somya@gmail.com>
2016-12-11 02:09:32 -0800

    Fixing view report status update
    done

--- app/src/main/java/com/ireport/activities/ViewReportActivity.java
+++ app/src/main/java/com/ireport/activities/ViewReportActivity.java
@@ -31,7 +31,8 @@ import com.ireport.model.ReportData;
 
 import java.util.ArrayList;
 
-public class ViewReportActivity extends AppCompatActivity implements ICallbackActivity {
+public class ViewReportActivity extends AppCompatActivity implements ICallbackActivity,
+        RadioGroup.OnCheckedChangeListener {
     public static String TAG = "ViewReportActivity";
     Geocoder geocoder;
 
@@ -44,7 +45,7 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
     private RadioGroup.OnCheckedChangeListener onCheckedChangeListener;
 
     // for status logic
-    private ReportData reportData;
+    private ReportData reportData = null;
     private String litterStatus;
 
     // handlers
@@ -59,6 +60,9 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_report);
 
+        // test code:
+//        ctx.getCurrentLoggedInUser().setOfficial(true);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.viewReportToolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(Color.BLACK);
@@ -67,9 +71,19 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
 
         String itemIdiInMongo = getIntent().getStringExtra("report_id_in_mongo");
         Log.d(TAG, "Item's mongo id is: " + itemIdiInMongo);
-        getReportByIdHandler = new GetReportByIdHandler(this, "view_report_activity", itemIdiInMongo);
-        getReportByIdHandler.getReportForReportId(getApplicationContext());
+        radioGroupStatus = (RadioGroup) findViewById(R.id.radio_group_status);
+        radioGroupStatus.setEnabled(false);
 
+        if (!ctx.getCurrentLoggedInUser().isOfficial()) {
+            CurrentLocationUtil.getCurrentLocation(ViewReportActivity.this,ctx);
+        }
+
+        getReportByIdHandler = new GetReportByIdHandler(
+                this,
+                "view_report_activity",
+                itemIdiInMongo
+        );
+        getReportByIdHandler.getReportForReportId(getApplicationContext());
 
         //Set all assets
         mDescriptionTextView = (TextView) findViewById(R.id.litterDescTV);
@@ -80,33 +94,41 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
         mSeverityTextView = (TextView) findViewById(R.id.litterSeverityTV);
         mLocationTextView = (TextView) findViewById(R.id.litterLocationTV);
 
-        radioGroupStatus = (RadioGroup) findViewById(R.id.radio_group_status);
         mUpdateReportBtn = (FloatingActionButton) findViewById(R.id.update_report_button);
 
         mImageLayout = (LinearLayout)findViewById(R.id.image_layout);
 
-
-        onCheckedChangeListener = new RadioGroup.OnCheckedChangeListener()
+        onCheckedChangeListener = this;
+/*                new RadioGroup.OnCheckedChangeListener()
         {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-//                Get current location
+                //Get current location
                 Location currLocation = new Location("");
                 Location reportLocation = new Location("");
-
 
                 Float distance = 0f;
 
                 if(ctx.getCurrentLocation() == null) {
-                    Toast.makeText(getBaseContext(), "Could not get the user's current location. Retrying....", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(
+                            getBaseContext(),
+                            "Could not get the user's current location. Retrying....",
+                            Toast.LENGTH_SHORT
+                    ).show();
+
                     CurrentLocationUtil.getCurrentLocation(ViewReportActivity.this,ctx);
                     return;
                 } else if (reportLocation.equals(null)) {
-                    Toast.makeText(getBaseContext(), "Could not get report location", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getBaseContext(),
+                            "Could not get report location",
+                            Toast.LENGTH_SHORT
+                    ).show();
                     return;
                 } else {
                     float[] dist = new float[1];
+
                     currLocation.setLatitude(ctx.getCurrentLocation().getLatitude());
                     currLocation.setLongitude(ctx.getCurrentLocation().getLongitude());
+
                     reportLocation.setLatitude(reportData.getLocation().getLatitude());
                     reportLocation.setLongitude(reportData.getLocation().getLongitude());
 
@@ -115,20 +137,26 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
 
                 }
 
-
-
-
                 // checkedId is the RadioButton selected
                 String oldLitterStatus = litterStatus;
-                String newLitterStatus = ((RadioButton) findViewById(radioGroupStatus.getCheckedRadioButtonId())).getText().toString();
+                String newLitterStatus = ((RadioButton) findViewById(radioGroupStatus.
+                        getCheckedRadioButtonId())).
+                        getText().
+                        toString();
                 Log.v(TAG,"Old Status = " + oldLitterStatus + " New Status = " + newLitterStatus);
-
 
                 if(distance > 30f) {
                     Log.v(TAG,"User not within range!!");
                     Log.v(TAG,"Distance = " + distance);
-                    Toast.makeText(getBaseContext(), "User must be within 30ft of radius from the posted location!", Toast.LENGTH_SHORT).show();
+
+                    Toast.makeText(
+                            getBaseContext(),
+                            "User must be within 30ft of radius from the posted location!",
+                            Toast.LENGTH_SHORT
+                    ).show();
+
                     radioGroupStatus.setOnCheckedChangeListener(null);
+
                     if(oldLitterStatus.equals("still_there")) {
                         Log.v(TAG,"still_there im here");
                         radioGroupStatus.check(R.id.radio_present);
@@ -147,7 +175,87 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
                 }
             }
         };
+        */
     }
+
+    private float getReportDistance(ReportData data) {
+        Float distance = 0f;
+
+        //Get current location
+        Location currLocation = new Location("");
+        Location reportLocation = new Location("");
+
+        float[] dist = new float[1];
+
+        currLocation.setLatitude(ctx.getCurrentLocation().getLatitude());
+        currLocation.setLongitude(ctx.getCurrentLocation().getLongitude());
+
+        reportLocation.setLatitude(data.getLocation().getLatitude());
+        reportLocation.setLongitude(data.getLocation().getLongitude());
+
+        distance = currLocation.distanceTo(reportLocation) * 0.328084f;
+        return distance;
+    }
+
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if(ctx.getCurrentLocation() == null && !ctx.getCurrentLoggedInUser().isOfficial()) {
+            CurrentLocationUtil.getCurrentLocation(ViewReportActivity.this,ctx);
+        }
+        if (reportData == null) {        // redundant check
+            Toast.makeText(getBaseContext(),
+                    "Could not get report Data",
+                    Toast.LENGTH_SHORT
+            ).show();
+            return;
+        }
+
+        if (ctx.getCurrentLoggedInUser().isOfficial()) {
+
+
+        } else {
+            // user case
+            float distance = this.getReportDistance(this.reportData);
+            Log.v(TAG, "Setting distance to " + distance);
+
+            // checkedId is the RadioButton selected
+            String oldLitterStatus = litterStatus;
+            String newLitterStatus = ((RadioButton) findViewById(radioGroupStatus.
+                    getCheckedRadioButtonId())).
+                    getText().
+                    toString();
+            Log.v(TAG, "Old Status = " + oldLitterStatus + " New Status = " + newLitterStatus);
+
+            if (distance > 30f) {
+                Log.v(TAG, "User not within range!!");
+                Log.v(TAG, "Distance = " + distance);
+
+                Toast.makeText(
+                        getBaseContext(),
+                        "User must be within 30ft of radius from the posted location!",
+                        Toast.LENGTH_SHORT
+                ).show();
+
+                radioGroupStatus.setOnCheckedChangeListener(null);
+
+                if (oldLitterStatus.equals("still_there")) {
+                    Log.v(TAG, "still_there im here");
+                    radioGroupStatus.check(R.id.radio_present);
+                } else if (oldLitterStatus.equals("removal_claimed")) {
+                    Log.v(TAG, "removal_claimed im here");
+                    radioGroupStatus.check(R.id.radio_claimed);
+                } else if (oldLitterStatus.equals("removal_confirmed")) {
+                    Log.v(TAG, "removal_confirmed im here");
+                    radioGroupStatus.check(R.id.radio_confirmed);
+                }
+                radioGroupStatus.setOnCheckedChangeListener(this);
+            } else {
+                litterStatus = newLitterStatus;
+                reportData.setStatus(litterStatus);
+                Log.v(TAG, "Updating status to " + litterStatus);
+            }
+        }
+    }
+
 
     private ArrayList<Bitmap> getImage(String imageString) {
         String ResponseImageArrayString[] = imageString.split(",");
@@ -161,7 +269,6 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
         return responseImages;
     }
 
-
     @Override
     public void onPostProcessCompletion(Object responseObj, String identifier, boolean isSuccess) {
         Log.d("VIEWING", "We have something");
@@ -172,6 +279,7 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
     }
 
     public void updatePageWithNewData(ReportData testReportData) {
+        radioGroupStatus.setEnabled(true);
         reportData = testReportData;
         // Load description, datetime, screenname, email, size, severity and location
         mDescriptionTextView.setText(testReportData.getDescription());
@@ -185,8 +293,12 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
         mUpdateReportBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateReportByIdHandler = new UpdateReportByIdHandler(ViewReportActivity.this,
-                        "view_report_activity", reportData.getReportId(), reportData.getStatus());
+                updateReportByIdHandler = new UpdateReportByIdHandler(
+                        ViewReportActivity.this,
+                        "view_report_activity",
+                        reportData.getReportId(),
+                        reportData.getStatus()
+                );
                 updateReportByIdHandler.updateReportForReportId(getApplicationContext());
                 Intent upIntent = getParentActivityIntent();
                 startActivity(upIntent);
@@ -215,21 +327,34 @@ public class ViewReportActivity extends AppCompatActivity implements ICallbackAc
         }
         /////////////////////////////////////////////
 
-
-
         //Status
         litterStatus = testReportData.getStatus();
-        if(litterStatus.equals("still_there")) {
-            radioGroupStatus.check(R.id.radio_present);
-        } else if (litterStatus.equals("removal_claimed")) {
-            radioGroupStatus.check(R.id.radio_claimed);
-        } else if(litterStatus.equals("removal_confirmed")) {
-            radioGroupStatus.check(R.id.radio_confirmed);
-        } else {
-            Log.v(TAG, "Invalid status found!");
+        boolean is_official = ctx.getCurrentLoggedInUser().isOfficial();
+        switch (litterStatus) {
+            case "still_there":
+                radioGroupStatus.check(R.id.radio_present);
+                if (is_official) {
+                    radioGroupStatus.getChildAt(2).setEnabled(false);
+                } else {
+                    radioGroupStatus.getChildAt(1).setEnabled(false);
+                }
+                break;
+            case "removal_claimed":
+                radioGroupStatus.check(R.id.radio_claimed);
+                if (is_official) {
+                    radioGroupStatus.getChildAt(0).setEnabled(false);
+                    radioGroupStatus.getChildAt(2).setEnabled(false);
+                }
+                break;
+            case "removal_confirmed":
+                radioGroupStatus.check(R.id.radio_confirmed);
+                radioGroupStatus.getChildAt(0).setEnabled(false);
+                radioGroupStatus.getChildAt(1).setEnabled(false);
+                break;
+            default:
+                Log.v(TAG, "Invalid status found!");
         }
 
         radioGroupStatus.setOnCheckedChangeListener(onCheckedChangeListener);
-
     }
 }
