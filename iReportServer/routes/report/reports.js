b85db64fe4e51aa7ac56539341ccc14152aa6e34
Anshuman Tripathi <anshuman.tripathi305@gmail.com>
2016-12-10 19:22:16 -0800

    Server :- Filter reports; Android :- Gradle Fix

--- iReportServer/routes/report/reports.js
+++ iReportServer/routes/report/reports.js
@@ -7,7 +7,7 @@ var email = require('../email_notify');
 
 exports.addReport = function (req, res) {
     var query = req.body;
-    query['status'] =  'still_there';
+    query['status'] = 'still_there';
     var report = new Report(query);
     var jsonResponse;
     console.log(report);
@@ -79,6 +79,7 @@ exports.getReports = function (req, res) {
     } else if (req.body.hasOwnProperty("location".toLowerCase())) {
         query = {location: req.body.location}
     }
+    console.log(query);
     Report.find(query).lean().exec(function (err, report) {
         if (err) {
             console.log("Error occured in fetching reports: " + err);
@@ -171,6 +172,23 @@ exports.getReportById = function (req, res) {
             };
         } else {
             console.log(report);
+            for (var i = 0; i < report.length; i++) {
+                var d = new Date(report[i].timestamp),
+                    month = '' + (d.getMonth() + 1),
+                    day = '' + d.getDate(),
+                    year = d.getFullYear(),
+                    hour = '' + d.getHours(),
+                    minutes = '' + d.getMinutes();
+
+                if (month.length < 2) month = '0' + month;
+                if (day.length < 2) day = '0' + day;
+                if (hour.length < 2) hour = '0' + hour;
+                if (minutes.length < 2) minutes = '0' + minutes;
+
+                report[i].timestamp = [month, day, year].join("-");
+                report[i].timestamp += " ";
+                report[i].timestamp += [hour, minutes].join(":");
+            }
             jsonResponse = {
                 statusCode: 200,
                 data: report
@@ -247,8 +265,40 @@ exports.updateReportStatus = function (req, res) {
             });
         }
     });
-    // res.send({
-    //     statusCode: 200,
-    //     data: "Response"
-    // });
+};
+
+exports.filterReports = function (req, res) {
+    var queryEmail = req.body.query_email;
+    var jsonResponse = {};
+    console.log(req.body);
+    Report.find({"user_email": queryEmail}, function (err, userReport) {
+        if (err) {
+            console.log("Some Error Occured: " + err);
+            jsonResponse = {
+                statusCode: 500,
+                data: err
+            };
+        } else if (userReport.length < 1) {
+            console.log("No report found from user");
+            jsonResponse = {
+                statusCode: 400,
+                data: "No User found"
+            }
+        } else {
+            var filterReports = [];
+            var requestStatus = req.body.query_status;
+            var temp = requestStatus.split(",");
+            for (var i = 0; i < userReport.length; i++) {
+                if (!temp.indexOf(userReport[i].status)) {
+                    console.log(userReport[i]);
+                    filterReports.push(userReport[i]);
+                }
+            }
+            jsonResponse = {
+                statusCode: 200,
+                data: filterReports
+            };
+        }
+        res.send(jsonResponse);
+    });
 };
